f0fb1ebc4f6b0fe638ee7643e101dfe583e99d65
Leonardo Felipe Ribas Ferreira <Leonardo_felipe1@outlook.com>
2025-04-14 17:29:53 -0300

    Update models.py

--- backend/models.py
+++ backend/models.py
@@ -1,1 +1,12 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100))
+    content = db.Column(db.Text)
+
+    def to_dict(self):
+        return {'id': self.id, 'title': self.title, 'content': self.content}
 
